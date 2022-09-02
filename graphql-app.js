4e356684ac7c96adbdd272b3170e41db75385d6b
Parker Chen <parker.chen@fairfriend.com.tw>
2022-09-02 16:03:58 +0800

    update

--- graphql-app.js
+++ graphql-app.js
@@ -9,46 +9,54 @@ import { RandomDieQuery } from '@/schema/RandomDie';
 import { MessageMutation, MessageQuery } from '@/schema/Message';
 import { UserListMutation, UserListQuery } from '@/schema/UserList';
 
+const queryList = {
+  hello: {
+    type: GraphQLString,
+    resolve: () => {
+      return 'Hello world!';
+    },
+  },
+  quoteOfTheDay: {
+    type: GraphQLString,
+    resolve: () => {
+      return Math.random() < 0.5
+        ? 'Take it easy'
+        : 'Salvation lies within';
+    },
+  },
+  random: {
+    type: GraphQLFloat,
+    resolve: () => {
+      return Math.random();
+    },
+  },
+  ...RandomDieQuery,
+  ...MessageQuery,
+  ...UserListQuery
+};
+
+const mutationList = {
+  ...MessageMutation,
+  ...UserListMutation
+};
+
 export default function GraphQL(graphiql) {
   // https://graphql.cn/graphql-js/graphql-clients/
-  const queryList = new GraphQLObjectType({
+  const query = new GraphQLObjectType({
     name: 'Query',
     fields: {
-      hello: {
-        type: GraphQLString,
-        resolve: () => {
-          return 'Hello world!';
-        },
-      },
-      quoteOfTheDay: {
-        type: GraphQLString,
-        resolve: () => {
-          return Math.random() < 0.5
-            ? 'Take it easy'
-            : 'Salvation lies within';
-        },
-      },
-      random: {
-        type: GraphQLFloat,
-        resolve: () => {
-          return Math.random();
-        },
-      },
-      ...RandomDieQuery,
-      ...MessageQuery,
-      ...UserListQuery
+      ...queryList
     }
   });
-  const mutationList = new GraphQLObjectType({
+  const mutation = new GraphQLObjectType({
     name: 'Mutation',
     fields: {
-      ...MessageMutation,
-      ...UserListMutation,
+      ...mutationList
     }
   });
   const schema = new GraphQLSchema({
-    query: queryList,
-    mutation: mutationList,
+    query,
+    mutation,
   });
 
   return graphqlHTTP({
